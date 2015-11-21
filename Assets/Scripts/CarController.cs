56ce43a9f3e29b481ba466695ff2fc60f02ab636
daybson <kaz-29@hotmail.com>
2015-11-21 13:46:55 -0200

    Innitial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,51 @@
+﻿/**
+    Criado por: Daybson Paisante
+    Contato: daybson.paisante@outlook.com
+    Visualizado em: https://github.com/daybson/Unity5CarTutorial
+*/
+
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField]
+    private WheelController[] wheelControllers;
+    [SerializeField]
+    private float maxSteerAngle;
+    [SerializeField]
+    private Transform massCenter;    
+    [SerializeField]
+    private float engineTorque = 2500;
+    [SerializeField]
+    private float brakeTorque = 900;
+    [SerializeField]
+    private Rigidbody rigidbody;
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.centerOfMass = massCenter.localPosition;
+        wheelControllers = GetComponentsInChildren<WheelController>();
+    }
+
+    private void FixedUpdate()
+    {
+        var steer = maxSteerAngle * Input.GetAxis("Horizontal");
+        var torque = engineTorque * Input.GetAxis("Vertical");
+        var brake = Input.GetKey(KeyCode.Space);
+
+        for (int i = 0; i < wheelControllers.Length; i++)
+        {
+            if (wheelControllers[i].ReceiveSteer)
+                wheelControllers[i].WheelCollider.steerAngle = steer;
+
+            if (wheelControllers[i].ReceiveTorque)
+                wheelControllers[i].WheelCollider.motorTorque = torque;
+
+            if (brake && wheelControllers[i].ReceiveBrake)
+                wheelControllers[i].WheelCollider.brakeTorque = this.brakeTorque;
+            else
+                wheelControllers[i].WheelCollider.brakeTorque = 0;
+        }
+    }    
+}
---
+++ Assets/Scripts/WheelController.cs
@@ -0,0 +1,52 @@
+﻿/**
+    Criado por: Daybson Paisante
+    Contato: daybson.paisante@outlook.com
+    Visualizado em: https://github.com/daybson/Unity5CarTutorial
+*/
+
+using UnityEngine;
+
+[RequireComponent(typeof(WheelCollider))]
+public class WheelController : MonoBehaviour
+{
+    [SerializeField]
+    private WheelCollider wheelCollider;
+    [SerializeField]
+    private Transform wheelMesh;
+    [SerializeField]
+    private bool receiveMotorTorque;
+    [SerializeField]
+    private bool receiveSteer;
+    [SerializeField]
+    private bool receiveBrake;
+    public bool ReceiveSteer { get { return receiveSteer; } }
+    public bool ReceiveTorque { get { return receiveMotorTorque; } }
+    public bool ReceiveBrake { get { return receiveBrake; } }
+
+    public WheelCollider WheelCollider
+    {
+        get
+        {
+            return wheelCollider;
+        }
+    }
+
+    private void Awake()
+    {
+        wheelCollider = GetComponent<WheelCollider>();
+    }
+
+    private void FixedUpdate()
+    {
+        RotateMesh();
+    }
+
+    private void RotateMesh()
+    {
+        Quaternion quat;
+        Vector3 position;
+        WheelCollider.GetWorldPose(out position, out quat);
+        wheelMesh.transform.position = position;
+        wheelMesh.transform.rotation = quat;
+    }
+}
